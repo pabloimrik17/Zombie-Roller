4215e051fc76dd6df6111590cddcb5b3cc00eb26
Pablo <pabloimrik17@gmail.com>
2017-01-15 20:32:39 +0100

    Finished score system, and camera adjustment

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public GameManager gameManager;
+
+    public AudioClip hitSound;
+
+    AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other) {
+        gameManager.AddPoint();
+        audioSource.PlayOneShot(hitSound);
+    }
+}
